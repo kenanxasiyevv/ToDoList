77ee39d32c3f527b16b06382def500bc102cc294
Kanan Xasiyev <121182410+kenanxasiyevv@users.noreply.github.com>
2023-04-10 18:07:06 +0400

    Add files via upload

--- ToDoList/js/main.js
+++ ToDoList/js/main.js
@@ -4,25 +4,42 @@ let number = document.getElementById('h11');
 
 let count = 0;
 
+
 function Add() {
     count++;
-    let a = document.createElement('h3')
+
+    let a = document.createElement('span')
     let b = document.createElement('button')
     let div = document.createElement('div')
     let inp = document.getElementById('inp').value
+    let disp = document.getElementById('disp')
     let inp1 = inp.toUpperCase()
     div.className = 'task'
 
+   
+
+    if (count > 0) {
+        disp.style.display = 'none'
+    }
+
+    
+    
     div.append(a, b)
     taskslist.append(div)
-
+    
     b.innerHTML = 'Delete';
     a.innerHTML = inp1;
     number.innerHTML = count;
-
+    
+    
     b.onclick = function () {
+        
         count--;
         div.remove();
         number.innerHTML = count;
+        if (count === 0) {
+            disp.style.display = 'block'
+        }
+        
     }
 }
